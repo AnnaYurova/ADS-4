fd133378f478fe5270d55dcbde08dbf6fb86980e
AnnaYurova <94358663+AnnaYurova@users.noreply.github.com>
2022-03-27 11:47:26 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,36 +1,37 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
     int count = 0;
-    for(int i = 0; i < len-1; i++){
-        for( int j = i+1; j < len; j++){
-            
-        if(arr[i] + arr[j] == value)
-            count ++;
+    for (int i = 0; i < len-1; i++) {
+        for (int j = i+1; j < len; j++) { 
+        if (arr[i] + arr[j] == value)
+            count++;
         }
     }
     return count;
 }
 int countPairs2(int *arr, int len, int value) {
   int count = 0;
-  int i = len, 
+  int i = len,
   j = len - 1; 
     for (int i = len - 1; arr[i] > value; i--)
       j--;
-    for ( int num = 0; num <= len; num++){
-      if(arr[num] > value / 2)
+    for (int num = 0; num <= len; num++) {
+      if(arr[num] > value / 2) {
         break;
+      }
           for (int right = j; right > num; right--) {
             if (arr[num] + arr[right] == value) {
               count++;
               }
-            if (arr[num] + arr[right] < value)
+            if (arr[num] + arr[right] < value) {
               break;
+            }
     }
     return count;
 }
 int countPairs3(int *arr, int len, int value) {
   int count = 0;
-int pr = len - 1;
+  int pr = len - 1;
   for (int i = 0; i < len - 1; i++) {
     int check = 0;
     int le = i + 1;
